4ba6e5a4d1ae989a603612cf3909747b5db81f6f
diegomontoya <diegomontoya@users.noreply.github.com>
2023-05-16 19:43:02 +0800

    bad tabs

--- src/gptq_triton/fused_attention.py
+++ src/gptq_triton/fused_attention.py
@@ -44,9 +44,9 @@ def rotate_half_kernel(
     position_id = tl.load(position_ids_ptr + batch * position_ids_batch_stride + seq)
     # As sometimes happens, just calculating this on the fly is faster than loading it from memory.
     # Use `tl.libdevice.exp` rather than `tl.exp` -- the latter is less accurate.
-	# triton 2.1.0 moved tl.libdevice.exp to tl.math.exp
-	if hasattr(tl, 'mathlib'):
-		freq = tl.mathlib.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
+    # triton 2.1.0 moved tl.libdevice.exp to tl.math.exp
+    if hasattr(tl, 'mathlib'):
+        freq = tl.mathlib.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
     else:
         freq = tl.libdevice.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
 
@@ -78,8 +78,10 @@ def triton_rotate_half_(qk, position_ids, config=None):
     assert qk.stride(4) == 1
     assert position_ids.shape == (batch_size, seq_len)
     assert position_ids.stride(1) == 1, 'position_ids must be contiguous in the last dimension'
-    assert (2 * num_heads) % config['BLOCK_HEIGHT'] == 0, f'number of rows not evenly divisible by {config["BLOCK_HEIGHT"]}'
-    assert (head_dim // 2) % config['BLOCK_WIDTH'] == 0, f'number of columns ({head_dim // 2}) not evenly divisible by {config["BLOCK_WIDTH"]}'
+    assert (2 * num_heads) % config[
+        'BLOCK_HEIGHT'] == 0, f'number of rows not evenly divisible by {config["BLOCK_HEIGHT"]}'
+    assert (head_dim // 2) % config[
+        'BLOCK_WIDTH'] == 0, f'number of columns ({head_dim // 2}) not evenly divisible by {config["BLOCK_WIDTH"]}'
 
     qk_by_seq = qk.view(batch_size * seq_len, 2 * num_heads * head_dim)
     grid = (qk_by_seq.shape[0], (2 * num_heads // config['BLOCK_HEIGHT']) * (head_dim // 2 // config['BLOCK_WIDTH']))
@@ -102,142 +104,143 @@ def triton_rotate_half_(qk, position_ids, config=None):
 
 
 def make_quant_attn(model):
-	"""
+    """
 	Replace all LlamaAttention modules with QuantLlamaAttention modules, fusing the q, k, v projections.
 	"""
-	for name, m in model.named_modules():
-		if not isinstance(m, LlamaAttention):
-			continue
+    for name, m in model.named_modules():
+        if not isinstance(m, LlamaAttention):
+            continue
 
-		q_proj = m.q_proj
-		k_proj = m.k_proj
-		v_proj = m.v_proj
+        q_proj = m.q_proj
+        k_proj = m.k_proj
+        v_proj = m.v_proj
 
-		qweights = torch.cat([q_proj.qweight, k_proj.qweight, v_proj.qweight], dim=1)
-		qzeros = torch.cat([q_proj.qzeros, k_proj.qzeros, v_proj.qzeros], dim=1)
-		scales = torch.cat([q_proj.scales, k_proj.scales, v_proj.scales], dim=1)
+        qweights = torch.cat([q_proj.qweight, k_proj.qweight, v_proj.qweight], dim=1)
+        qzeros = torch.cat([q_proj.qzeros, k_proj.qzeros, v_proj.qzeros], dim=1)
+        scales = torch.cat([q_proj.scales, k_proj.scales, v_proj.scales], dim=1)
 
-		qkv_layer = QuantLinear(q_proj.bits, q_proj.groupsize, q_proj.infeatures, q_proj.outfeatures + k_proj.outfeatures + v_proj.outfeatures, bias=False)
-		qkv_layer.qweight = qweights
-		qkv_layer.qzeros = qzeros
-		qkv_layer.scales = scales
-		qkv_layer.bias = None
+        qkv_layer = QuantLinear(q_proj.bits, q_proj.groupsize, q_proj.infeatures,
+                                q_proj.outfeatures + k_proj.outfeatures + v_proj.outfeatures, bias=False)
+        qkv_layer.qweight = qweights
+        qkv_layer.qzeros = qzeros
+        qkv_layer.scales = scales
+        qkv_layer.bias = None
 
-		# We're dropping the rotary embedding layer m.rotary_emb here. We don't need it in the triton branch.
-		attn = QuantLlamaAttention(m.config, qkv_layer, m.o_proj)
+        # We're dropping the rotary embedding layer m.rotary_emb here. We don't need it in the triton branch.
+        attn = QuantLlamaAttention(m.config, qkv_layer, m.o_proj)
 
-		if '.' in name:
-			parent_name = name.rsplit('.', 1)[0]
-			child_name = name[len(parent_name) + 1:]
-			parent = model.get_submodule(parent_name)
-		else:
-			parent_name = ''
-			parent = model
-			child_name = name
+        if '.' in name:
+            parent_name = name.rsplit('.', 1)[0]
+            child_name = name[len(parent_name) + 1:]
+            parent = model.get_submodule(parent_name)
+        else:
+            parent_name = ''
+            parent = model
+            child_name = name
 
-		#print(f"Replacing {name} with quant_attn; parent: {parent_name}, child's name: {child_name}")
+        # print(f"Replacing {name} with quant_attn; parent: {parent_name}, child's name: {child_name}")
 
-		setattr(parent, child_name, attn)
+        setattr(parent, child_name, attn)
 
 
 class QuantLlamaAttention(nn.Module):
-	"""
+    """
 	Modified version of LlamaAttention that fuses the q, k, v projections.
 	"""
 
-	def __init__(
-		self,
-		config: LlamaConfig,
-		qkv_proj,
-		o_proj
-	):
-		super().__init__()
-		self.config = config
-		self.hidden_size = config.hidden_size
-		self.num_heads = config.num_attention_heads
-		self.head_dim = self.hidden_size // self.num_heads
-		self.max_position_embeddings = config.max_position_embeddings
+    def __init__(
+            self,
+            config: LlamaConfig,
+            qkv_proj,
+            o_proj
+    ):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.hidden_size // self.num_heads
+        self.max_position_embeddings = config.max_position_embeddings
 
-		if (self.head_dim * self.num_heads) != self.hidden_size:
-			raise ValueError(
-				f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
-				f" and `num_heads`: {self.num_heads})."
-			)
-		self.qkv_proj = qkv_proj
-		self.o_proj = o_proj
+        if (self.head_dim * self.num_heads) != self.hidden_size:
+            raise ValueError(
+                f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
+                f" and `num_heads`: {self.num_heads})."
+            )
+        self.qkv_proj = qkv_proj
+        self.o_proj = o_proj
 
-	def forward(
-		self,
-		hidden_states: torch.Tensor,
-		attention_mask: Optional[torch.Tensor] = None,
-		position_ids: Optional[torch.LongTensor] = None,
-		past_key_value: Optional[Tuple[torch.Tensor]] = None,
-		output_attentions: bool = False,
-		use_cache: bool = False,
-	) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-		"""Input shape: Batch x Time x Channel"""
-		bsz, q_len, _ = hidden_states.size()
+    def forward(
+            self,
+            hidden_states: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            past_key_value: Optional[Tuple[torch.Tensor]] = None,
+            output_attentions: bool = False,
+            use_cache: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        """Input shape: Batch x Time x Channel"""
+        bsz, q_len, _ = hidden_states.size()
 
-		qkv_states = self.qkv_proj(hidden_states)
-		qkv_states = qkv_states.view(bsz, q_len, 3, self.num_heads, self.head_dim)
+        qkv_states = self.qkv_proj(hidden_states)
+        qkv_states = qkv_states.view(bsz, q_len, 3, self.num_heads, self.head_dim)
 
-		# This updates the query and key states in-place, saving VRAM.
-		triton_rotate_half_(qkv_states[:, :, :2], position_ids)
+        # This updates the query and key states in-place, saving VRAM.
+        triton_rotate_half_(qkv_states[:, :, :2], position_ids)
 
-		query_states, key_states, value_states = torch.split(qkv_states, 1, dim=2)
-		del qkv_states
+        query_states, key_states, value_states = torch.split(qkv_states, 1, dim=2)
+        del qkv_states
 
-		query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-		key_states = key_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-		value_states = value_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
 
-		kv_seq_len = q_len
-		if past_key_value is not None:
-			kv_seq_len += past_key_value[0].shape[-2]
+        kv_seq_len = q_len
+        if past_key_value is not None:
+            kv_seq_len += past_key_value[0].shape[-2]
 
-			# reuse k, v, self_attention
-			key_states = torch.cat([past_key_value[0], key_states], dim=2)
-			value_states = torch.cat([past_key_value[1], value_states], dim=2)
-		
-		if use_cache:
-			# Since qkv_proj is fused, query_states etc will hold a reference to the original qkv_states tensor
-			# which can cause excessive memory usage by the cache. `contiguous` is a convenient way to workaround this.
-			key_states = key_states.contiguous()
-			value_states = value_states.contiguous()
+            # reuse k, v, self_attention
+            key_states = torch.cat([past_key_value[0], key_states], dim=2)
+            value_states = torch.cat([past_key_value[1], value_states], dim=2)
 
-			query_states = query_states.contiguous()
+        if use_cache:
+            # Since qkv_proj is fused, query_states etc will hold a reference to the original qkv_states tensor
+            # which can cause excessive memory usage by the cache. `contiguous` is a convenient way to workaround this.
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
 
-		past_key_value = (key_states, value_states) if use_cache else None
+            query_states = query_states.contiguous()
 
-		attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        past_key_value = (key_states, value_states) if use_cache else None
 
-		if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
-			raise ValueError(
-				f"Attention weights should be of size {(bsz * self.num_heads, q_len, kv_seq_len)}, but is"
-				f" {attn_weights.size()}"
-			)
+        attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
-		if attention_mask is not None:
-			if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
-				raise ValueError(
-					f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
-				)
-			attn_weights = attn_weights + attention_mask
-			attn_weights = torch.max(attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min))
+        if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+            raise ValueError(
+                f"Attention weights should be of size {(bsz * self.num_heads, q_len, kv_seq_len)}, but is"
+                f" {attn_weights.size()}"
+            )
 
-		# upcast attention to fp32
-		attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-		attn_output = torch.matmul(attn_weights, value_states)
+        if attention_mask is not None:
+            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                raise ValueError(
+                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                )
+            attn_weights = attn_weights + attention_mask
+            attn_weights = torch.max(attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min))
 
-		if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
-			raise ValueError(
-				f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
-				f" {attn_output.size()}"
-			)
+        # upcast attention to fp32
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        attn_output = torch.matmul(attn_weights, value_states)
 
-		del query_states, key_states, value_states
+        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+            raise ValueError(
+                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f" {attn_output.size()}"
+            )
 
-		attn_output = attn_output.transpose(1, 2).reshape(bsz, q_len, self.hidden_size)
-		attn_output = self.o_proj(attn_output)
+        del query_states, key_states, value_states
 
-		return attn_output, None, past_key_value
+        attn_output = attn_output.transpose(1, 2).reshape(bsz, q_len, self.hidden_size)
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
