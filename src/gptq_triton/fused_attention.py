ad45027c326cd1285e35ff56c07fa996fa6305ff
diegomontoya <diegomontoya@users.noreply.github.com>
2023-05-16 19:53:02 +0800

    test

--- src/gptq_triton/fused_attention.py
+++ src/gptq_triton/fused_attention.py
@@ -45,10 +45,10 @@ def rotate_half_kernel(
     # As sometimes happens, just calculating this on the fly is faster than loading it from memory.
     # Use `tl.libdevice.exp` rather than `tl.exp` -- the latter is less accurate.
     # triton 2.1.0 moved tl.libdevice.exp to tl.math.exp
-    if hasattr(tl, 'mathlib'):
-        freq = tl.mathlib.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
-    else:
-        freq = tl.libdevice.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
+    #if hasattr(tl, 'mathlib'):
+    freq = tl.mathlib.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
+    #else:
+    #    freq = tl.libdevice.exp((col + tl.arange(0, BLOCK_WIDTH)).to(tl.float32) * INV_BASE) * position_id
 
     cos = tl.cos(freq).to(tl.float32)
     sin = tl.sin(freq).to(tl.float32)
